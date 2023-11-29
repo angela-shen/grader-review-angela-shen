fcbd39086f6a9b1492a02eb6d0cda9deacc538ce
angela-shen <136782411+angela-shen@users.noreply.github.com>
2023-11-28 16:39:00 -0800

    what.

--- TestListExamples.java
+++ TestListExamples.java
@@ -18,4 +18,12 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+  @Test(timeout = 500)
+  public void testFilter() {
+    List<String> original = Arrays.asList("sun", "moon", "stars");
+    List<String> filtered = ListExamples.filter(original, new IsMoon());
+    List<String> expected = Arrays.asList("moon");
+    assertEquals(expected, filtered);
+  }
+
 }
